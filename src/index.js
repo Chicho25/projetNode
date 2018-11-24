c1bc96f2c8ee8f31d461e853de141dac1817d8a9
Pedro Arrieta <pedroarrieta25@hotmail.com>
2018-11-24 12:02:15 -0500

    node 2
    segundo del dia

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-minuto 11:53
-asi es la ConstantSourceNode
+const express = require('express'); 
+const path = require('path'); // para concatenar carpetas
+// initialitations
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views')); // para saber donde esta la carpeta views, ya que deberia estar en la raiz
+
+// middlewares
+
+
+//global variables
+
+
+// static files
+
+
+// server is listenning
+app.listen(app.get('port'), () =>{
+    console.log('El servidor esta escuchando en el puerto', app.get('port'));
+});
+
+//minuto tuto 19:19
