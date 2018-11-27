965d7acb9247a56e423b2b0f002af764f762a482
Chicho25 <pedroarrieta25@hotmail.com>
2018-11-26 20:46:15 -0500

    26-11-18

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/notes', (req, res) => {
-    res.send('Notas de la base de datos');
+const Note = require('../models/Notes');
+
+router.get('/notes/add', (req, res) => {
+  res.render('notes/new-notes');
+});
+
+router.post('/notes/new-notes', async (req, res) => {
+ const { title, description } = req.body;
+ const error = [];
+  if(!title){
+    error.push({text: 'Por favor escriba un titulo'});
+  }
+  if(!description){
+    error.push({text: 'Por favor escriba un Mensaje'});
+  }
+  if (error.length > 0) {
+    res.render('notes/new-notes', {
+      error, 
+      title, 
+      description
+    });
+  }else{
+    const newNote = new Note({title, description});
+    /*console.log(newNote);*/ // para ver que esta guardando
+    await newNote.save();
+    res.redirect('/notes');
+  }
+  
+});
+
+router.get('/notes', async (req, res) => {
+    const notes = await Note.find();
+    res.render('notes/all-notes', { notes });
   });
 
 module.exports = router;
